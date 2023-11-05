96cdf9c232c9923537d9c4ec993d897732f112b9
Nicholas Herrera <nickherrera1208@gmail.com>
2023-11-05 15:08:47 -0500

    Complete loops and iterating exercises.

--- loopsAndIterating/loopsAndIterating.js
+++ loopsAndIterating/loopsAndIterating.js
@@ -38,3 +38,68 @@
         console.log(i += 1);
       }    
       It logs 1 2 3 4 5 to the console. */
+
+/* 5. The following code uses a randomNumberBetween function to generate a number between its first and second arguments. It uses a while loop to try to generate 
+      a number greater than 2. Refactor the code so that you don't need to call randomNumberBetween from two different locations, lines 6 and 10. Do not change
+      the arguments you pass to randomNumberBetween. 
+
+      function randomNumberBetween(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+
+      let tries = 0;
+      let result = randomNumberBetween(1, 6);
+      tries += 1;
+
+      while (result <= 2) {
+        result = randomNumberBetween(1, 6);
+        tries += 1;
+      }
+
+      console.log('It took ' + String(tries) + ' tries to get a number greater than 2');    */
+      /* My answer
+
+      function randomNumberBetween(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+      
+      let tries = 0
+
+      do {
+        result = randomNumberBetween(1,6);
+        tries += 1
+      } while (result <= 2);
+
+      console.log('It took ' + String(tries) + ' tries to get a number greater than 2'); */
+
+/* 6. Reimplement the factorial function from exercise 2 using recursion. Once again, you may assume that the argument is always a positive integer. 
+
+      console.log(factorial(1));     // => 1
+      console.log(factorial(2));     // => 2
+      console.log(factorial(3));     // => 6
+      console.log(factorial(4));     // => 24
+      console.log(factorial(5));     // => 120
+      console.log(factorial(6));     // => 720
+      console.log(factorial(7));     // => 5040
+      console.log(factorial(8));     // => 40320 */ 
+
+     /* My version
+
+      function fact(n) {
+        if (n > 1) {
+          return n * fact(n-1);
+        } else {
+          return 1;
+        }
+      }
+      console.log(fact(5)); */ 
+
+    /* LS Version
+
+    function factorial(number) {
+      if (number === 1) {
+        return 1;
+      }
+
+    return number * factorial(number - 1);
+    } */
